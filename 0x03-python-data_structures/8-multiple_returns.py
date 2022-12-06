7228b305e0dfea416dea7f6f5cb024010e78d216
Alabeni Omosheye David <omosheye007@gmail.com>
2022-12-05 20:36:02 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_sentence = len(sentence)
-
-if (len_sentence == 0):
+     
+     if (len_sentence == 0):
         new_tuple = (len_sentence, None)
-    else:
+     else:
         new_tuple = (len_sentence, sentence[0])
 
-return(new_tuple)
+     return(new_tuple)
