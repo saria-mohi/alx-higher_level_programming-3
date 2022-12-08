af60a6c10c8e5df5928eb1778d2b1a6ee2dd8ab7
Alabeni Omosheye David <omosheye007@gmail.com>
2022-12-08 15:17:33 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,6 @@
-!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary.keys():
+        del a_dictionary[key]
+
     return a_dictionary
